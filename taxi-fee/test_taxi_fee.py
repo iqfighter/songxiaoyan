d5e205329ea07c6f4461966b0af41a395955a02b
Xiaoyan Song <XSong@slb.com>
2016-04-12 00:25:05 +0800

    add test framework

---
+++ taxi-fee/test_taxi_fee.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python  
+#encoding: utf-8  
+  
+import unittest  
+from taxi_fee import taxi_fee
+  
+class test_(unittest.TestCase):  
+      
+    def setUp(self):  
+        pass   
+    
+    def tearDown(self):  
+        pass        
+    
+    def test_taxi_fee(self):  
+        self.assertEqual(1, 1)         
+
+if __name__ =='__main__':  
+    unittest.main() 
