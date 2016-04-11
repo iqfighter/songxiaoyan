4dae0a6d8da9f26292766a2445c04e477050b206
Xiaoyan Song <XSong@slb.com>
2016-04-12 00:43:07 +0800

    (2-8]miles 0.8mao per mile

--- taxi-fee/taxi_fee.py
+++ taxi-fee/taxi_fee.py
@@ -4,5 +4,7 @@
 def taxi_fee(miles):
 	if (miles <= 0):
 		return 0
-	else:
-		return 6
+	elif (miles <= 2):
+		return 6   
+	elif (miles <= 8):
+		return round (6 + (miles - 2) * 0.8)
--- taxi-fee/test_taxi_fee.py
+++ taxi-fee/test_taxi_fee.py
@@ -18,5 +18,12 @@ class test_(unittest.TestCase):
     def test_given_less_than_2miles_should_charge_6(self):  
         self.assertEqual(taxi_fee(1), 6)
 
+    def test_given_equals_to_2miles_should_charge_6(self):  
+        self.assertEqual(taxi_fee(2), 6)
+
+    def test_given_greater_than_2miles_but_less_than_8miles_should_charge_6_plus_8mao_per_mile_then_round(self):  
+        self.assertEqual(taxi_fee(4), round (6 + (4-2) * 0.8))
+
+
 if __name__ =='__main__':  
     unittest.main() 
