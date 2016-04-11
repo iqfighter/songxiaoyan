2ed0b41b672c62156dbff3cdd1c193d883d8f90e
Xiaoyan Song <XSong@slb.com>
2016-04-12 00:44:34 +0800

    (2-8]miles 0.8mao per mile with 8miles testcase

--- taxi-fee/test_taxi_fee.py
+++ taxi-fee/test_taxi_fee.py
@@ -24,6 +24,8 @@ class test_(unittest.TestCase):
     def test_given_greater_than_2miles_but_less_than_8miles_should_charge_6_plus_8mao_per_mile_then_round(self):  
         self.assertEqual(taxi_fee(4), round (6 + (4-2) * 0.8))
 
+    def test_given_equals_to_8miles_should_charge_6_plus_8mao_per_mile_then_round(self):  
+        self.assertEqual(taxi_fee(8), round (6 + (8-2) * 0.8))      
 
 if __name__ =='__main__':  
     unittest.main() 
